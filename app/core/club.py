211e9b01e29b7722f38a9a5a39c6ff74d2be7865
qonstellations <aarush.a.pathak@gmail.com>
2026-01-07 10:38:07 +0530

    roles initialised empty on club creation + fixed imports

--- app/core/club.py
+++ app/core/club.py
@@ -7,14 +7,13 @@ import os
 def create_club(
     name: str,
     description: str,
-    roles: list,
     secret_key: str
 ) -> Club:
 
     load_dotenv()
     secret_club_key = os.getenv("SECRET_KEY")
     
-    if not all([name, description, roles, secret_key]):
+    if not all([name, description, secret_key]):
         raise ValueError("All Fields are compulsory")
     
     if secret_key != secret_club_key:
@@ -25,7 +24,7 @@ def create_club(
         name=name,
         description=description,
         created_at=datetime.now(),
-        roles=[]
+        roles=list() # No Roles directly on club creation, user adds later
     )
 
     new_club_id = db.clubs.insert_one(club.conv_to_doc()).inserted_id
--- main.py
+++ main.py
@@ -1,20 +1,18 @@
-import app.core.user as user
-import app.core.club as Club
+import app.core as core # fixed import to avoid identifier confusion
 
 def main():
-    #user = auth.register_user("aarush@gmail.com", "abc123", "abc123", "aarush", "pathak")
+    #user = core.user.register_user("aarush@gmail.com", "abc123", "abc123", "aarush", "pathak")
     
-    club = Club.create_club(
-        "Epoch", 
-        "This is an AI&ML club created for the betterment of the society",
-        ["lead",],
-        "epoch123")
-
     # inp_email = str(input("Enter email : "))
     # inp_pass = str(input("Enter password : "))
 
-    # if(auth.authenticate_user(inp_email, inp_pass)):
+    # if(core.user.authenticate_user(inp_email, inp_pass)):
     #     print("Logged in!")
+
+    club = core.club.create_club(
+        "Epoch", 
+        "This is an AI&ML club created for the betterment of the society",
+        "epoch123")
 
 if __name__ == "__main__":
     main()
